1bd943ab759155773834c6b9f1bb588e21fea2e2
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-04-03 20:49:04 +0900

    다크모드 스위치버튼 추가

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     const sidebar = document.getElementById("sidebar");
     const tabBar = document.querySelector(".tab-bar");
 
-    
+    const switchButton = document.getElementById("switch");
+    const main = document.querySelector("main");
+
+    // 사이드바 햄버거 버튼 토글
     menuToggle.addEventListener("click", () => {
         sidebar.classList.toggle("hidden");
         
@@ -15,6 +18,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     });
 
-    
+    // 다크모드 토글
+    switchButton.addEventListener("change", function () {
+        main.classList.toggle("dark-mode", this.checked);
+    });
 });
 
